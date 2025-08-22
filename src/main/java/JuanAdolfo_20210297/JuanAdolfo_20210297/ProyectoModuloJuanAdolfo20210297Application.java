01ccb2d5d73c07c7a7ad33cd9a11876de3115977
Juan <20210297@ricaldone.edu.sv>
2025-08-22 14:18:29 -0600

    hola 3

--- src/main/java/JuanAdolfo_20210297/JuanAdolfo_20210297/ProyectoModuloJuanAdolfo20210297Application.java
+++ src/main/java/JuanAdolfo_20210297/JuanAdolfo_20210297/ProyectoModuloJuanAdolfo20210297Application.java
@@ -1,6 +1,5 @@
 package JuanAdolfo_20210297.JuanAdolfo_20210297;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoModuloJuanAdolfo20210297Application {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.get("SERVER_PORT");
 		SpringApplication.run(ProyectoModuloJuanAdolfo20210297Application.class, args);
 	}
 
