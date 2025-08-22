dad9371bab952b5b27e2ed135c5a183c738cb0b6
Juan <20210297@ricaldone.edu.sv>
2025-08-22 15:29:45 -0600

    suficiente 2

--- src/main/java/JuanAdolfo_20210297/JuanAdolfo_20210297/ProyectoModuloJuanAdolfo20210297Application.java
+++ src/main/java/JuanAdolfo_20210297/JuanAdolfo_20210297/ProyectoModuloJuanAdolfo20210297Application.java
@@ -8,15 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoModuloJuanAdolfo20210297Application {
 
-	// Profesor: Mire no me servía el .env y el application.properties; no me agarraba
-	// Me dolía la cabeza y manejaba 0 ganas de pensar más duro porque ya la había probado
-	// y ya servía pero sin las variables del env.
-	// Le ruego me disculpe. Pero si funciona. Igual la rúbrica no dice nadota de usar el
-	// .env ey 🥺. Lo que le digo, todo funciona menos eso. Y qué feo estar todo el rato
-	// quebrándome la cabeza. Porque EN VERDAD no sé qué es lo que me hace falta, no recuerdo.
+    // Profesor: Mire no me servía el .env y el application.properties; no me agarraba
+    // Me dolía la cabeza y manejaba 0 ganas de pensar más duro porque ya la había probado
+    // y ya servía pero sin las variables del env.
+    // Le ruego me disculpe. Pero si funciona. Igual la rúbrica no dice nadota de usar el
+    // .env ey 🥺. Lo que le digo, todo funciona menos eso. Y qué feo estar todo el rato
+    // quebrándome la cabeza. Porque EN VERDAD no sé qué es lo que me hace falta, no recuerdo.
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectoModuloJuanAdolfo20210297Application.class, args);
-	}
+    // A pues no, ya me ayudó usted. Gracias ey disculpe que me sentía un poquito mal 😥
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+        );
+        SpringApplication.run(ProyectoModuloJuanAdolfo20210297Application.class, args);
+    }
 
 }
